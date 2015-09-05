29e8de8cced00d4c4eed834f915ace3a83ae8bf5
Paul Baxter <paulgrahambaxter@gmail.com.com>
2015-09-05 16:34:49 +0100

    Adding comments and undefined defintions for 4-8

--- Spec.hs
+++ Spec.hs
@@ -11,6 +11,23 @@ myButLast = undefined
 elementAt = undefined
 
 
+-- myLength : Find the number of elements of a list.
+myLength = undefined
+
+-- myReverse : Reverse a list
+myReverse = undefined
+
+-- isPalindrome : Find out whether a list is a palindrome. A palindrome can be read forward or backward.isPalindrome.
+isPalindrome = undefined
+
+-- my-flatten : Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements 
+data NestedList a = Elem a | List [NestedList a]
+my-flatten = undefined
+
+
+-- compress : Eliminate consecutive duplicates of list elements.
+compress = undefined
+
 main :: IO ()
 main = hspec $ do
 
@@ -31,3 +48,9 @@ main = hspec $ do
 			elementAt [1,2,3] 2 `shouldBe` (2 :: Int)
 		it "returns the Kth element of a list of Char" $ do
 			elementAt "haskell" 5 `shouldBe` ('e' :: Char) 
+
+	describe "myLength" $ do
+		it "returns the number of elemennt in a list of Int" $ do
+			myLength [123, 456, 789] `shouldBe` (2 :: Int)
+		it "returns the number of elements in a list of Char" $ do
+			myLength "Hello, world!" `shouldBe` ('e' :: Char) 
